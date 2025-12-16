251b2d86ab406bee3ad6431866d96d3b80be9300
qadir <aqadir1989@gmail.com>
2025-12-16 14:42:06 +0530

    installed-tailwind-css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: { extend: {} },
+  plugins: [],
+};
